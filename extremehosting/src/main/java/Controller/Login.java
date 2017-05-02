b43033b1864008d4a8ae63b291a541b3418ea9e8
iblackman <igor.blackman@gmail.com>
2017-05-02 20:10:12 -0300

    criando Session ao logar
    criacao da session e das variaveis authenticated e username

--- extremehosting/src/main/java/Controller/Login.java
+++ extremehosting/src/main/java/Controller/Login.java
@@ -14,6 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -28,6 +29,9 @@ public class Login extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         if(LoginDAO.validate(username, password)){
+            HttpSession session = request.getSession();
+            session.setAttribute("authenticated", true);
+            session.setAttribute("username", username);
             request.setAttribute("username", username);
             RequestDispatcher rd = request.getRequestDispatcher("main");
             rd.forward(request, response);
@@ -37,6 +41,19 @@ public class Login extends HttpServlet {
             RequestDispatcher rd = request.getRequestDispatcher("error.jsp");
             rd.include(request, response);
         }
+    }
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath()+"/");
     }
     /**
      * Returns a short description of the servlet.
